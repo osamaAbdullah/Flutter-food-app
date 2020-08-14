d825595063983547115eb73c120da7aec9883e35
osama <osama@aso.com>
2020-08-14 19:31:17 +0300

    list.dart improved more heading for state managment

--- lib/tabs/food/list.dart
+++ lib/tabs/food/list.dart
@@ -11,20 +11,33 @@ class ListTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, index) {
-        return ListTile(
-          leading: Image.asset(_foods[index]['image']),
-          title: Text(_foods[index]['title']),
-          subtitle: Text(_foods[index]['description']),
-          trailing: IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (BuildContext context) {
-                return EditPage(_foods[index], _editFood, index);
-              }));
+        return Dismissible(
+            key: Key(index.toString()),
+            background: Container(color: Colors.red),
+            onDismissed: (DismissDirection direction) {
+              print('dismissed direction is ${direction.toString()}');
             },
-          ),
-        );
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(_foods[index]['image']),
+                  ),
+                  title: Text(_foods[index]['title']),
+                  subtitle: Text(_foods[index]['description']),
+                  trailing: IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return EditPage(_foods[index], _editFood, index);
+                      }));
+                    },
+                  ),
+                ),
+                Divider(),
+              ],
+            ));
       },
       itemCount: _foods.length,
     );
